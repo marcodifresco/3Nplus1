a0e8f89b8e413abdff4656efc5fb57d13b3d3335
Marco Di Fresco <marco.difresco@gmail.com>
2022-11-08 14:09:04 +0100

    Preliminary version

--- src/3Nplus1.cpp
+++ src/3Nplus1.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int number = 0, counter = 0;
+    std::cout << "Insert number: ";
+    std::cin >> number;
+
+    while (number>1){
+        if ((number%2)!=0)
+            number=number*3+1;
+        else
+            number=number/2;
+        counter++;
+        std::cout << "Now number is: " << number << " after " << counter << " rounds.\n";
+    }
+
     return 0;
 }
