a93e567d7416591139523ff9a1ae6f50c9931849
Marco Di Fresco <marco.difresco@gmail.com>
2022-11-08 14:34:08 +0100

    Added more breathing room by using unsigned long instead of normal int.

--- src/3Nplus1.cpp
+++ src/3Nplus1.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 
 int main() {
-    int number = 0, counter = 0;
+    unsigned long int number = 0, counter = 0;
     std::cout << "Insert number: ";
     std::cin >> number;
 
     while (number>1){
-        if ((number%2)!=0)
-            number=number*3+1;
-        else
-            number=number/2;
+        if ((number % 2) != 0) {
+            number = number * 3 + 1;
+        } else {
+            number = number / 2;
+        }
         counter++;
         std::cout << "Now number is: " << number << " after " << counter << " rounds.\n";
     }
